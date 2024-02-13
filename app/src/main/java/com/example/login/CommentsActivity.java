886e1343ddc4af1349af6d50dec2ef1d988da8da
Sapir Yanai <sapir9233@gmail.com>
2024-02-13 19:48:51 +0200

    c1

--- app/src/main/java/com/example/login/CommentsActivity.java
+++ app/src/main/java/com/example/login/CommentsActivity.java
@@ -1,19 +1,27 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-//import com.example.login.adapters.CommentsAdapter;
-import com.example.login.entities.Post;
+
+import com.example.login.adapters.CommentsAdapter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommentsActivity extends AppCompatActivity {
 
-    private List<Post> posts;
+    private List<String> comments;
     private RecyclerView recyclerView;
-//    private CommentsAdapter adapter;
+    private CommentsAdapter adapter;
+    private EditText commentEditText;
+    private Button addCommentButton;
+    private Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +29,45 @@ public class CommentsActivity extends AppCompatActivity {
         setContentView(R.layout.comments);
 
         // Initialize RecyclerView and Adapter
-//        recyclerView = findViewById(R.id.commentsRecyclerView);
-//        adapter = new CommentsAdapter();
+        recyclerView = findViewById(R.id.commentsRecyclerView);
+        adapter = new CommentsAdapter();
 
         // Set layout manager to RecyclerView
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Set adapter to RecyclerView
-//        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(adapter);
+
+        // Initialize comments list
+        comments = new ArrayList<>();
 
         // Populate comments list with data
         populateComments();
+
+        // Get references to EditText and Add button
+        commentEditText = findViewById(R.id.commentEditText);
+        addCommentButton = findViewById(R.id.addCommentButton);
+
+        // Set OnClickListener for Add button
+        addCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add comment to the list
+                String newComment = commentEditText.getText().toString();
+                if (!newComment.isEmpty()) {
+                    comments.add(newComment);
+                    adapter.setComments(comments); // Update the RecyclerView
+                    commentEditText.setText(""); // Clear the EditText
+                }
+            }
+        });
+
+
     }
 
     // Method to populate comments list with data
     private void populateComments() {
-        // Dummy implementation for demonstration
-        List<String> dummyComments = new ArrayList<>();
-        dummyComments.add("This is the first comment.");
-        dummyComments.add("This is the second comment.");
-        dummyComments.add("This is the third comment.");
-
         // Add comments to the adapter
-//        adapter.setComments(dummyComments);
+        adapter.setComments(comments);
     }
 }
--- app/src/main/java/com/example/login/adapters/CommentsAdapter.java
+++ app/src/main/java/com/example/login/adapters/CommentsAdapter.java
@@ -1,58 +1,120 @@
-//package com.example.login.adapters;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.TextView;
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.example.login.R;
-//import com.example.login.entities.Post;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.CommentViewHolder> {
-//
-//    private List<String> comments;
-//
-//    // Constructor
-//    public CommentsAdapter() {
-//        this.comments = new ArrayList<>();
-//    }
-//
-//    // Method to set the list of comments
-//    public void setComments(List<String> comments) {
-//        this.comments = comments;
-//        notifyDataSetChanged(); // Notify RecyclerView that data has changed
-//    }
-//
-//    // ViewHolder class
-//    static class CommentViewHolder extends RecyclerView.ViewHolder {
-//        TextView textViewComment;
-//
-//        public CommentViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            textViewComment = itemView.findViewById(R.id.textViewComment); // Adjust to your layout's TextView ID
-//        }
-//    }
-//
-//    @NonNull
-//    @Override
-//    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.one_comment, parent, false);
-//        return new CommentViewHolder(itemView);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
-//        String comment = comments.get(position);
-//        holder.textViewComment.setText(comment);
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return comments.size();
-//    }
-//}
+package com.example.login.adapters;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.login.R;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.CommentViewHolder> {
+
+    private List<String> comments;
+
+    // Constructor
+    public CommentsAdapter() {
+        this.comments = new ArrayList<>();
+    }
+
+    // Method to set the list of comments
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+        notifyDataSetChanged(); // Notify RecyclerView that data has changed
+    }
+
+    // ViewHolder class
+    static class CommentViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewComment;
+        Button deleteButton;
+        Button editButton;
+        TextView userNameTextView;
+        ImageView profileImageView;
+
+        public CommentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewComment = itemView.findViewById(R.id.commentTextView);
+            userNameTextView = itemView.findViewById(R.id.displayNameTextView);
+            profileImageView = itemView.findViewById(R.id.profileImageView);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
+            editButton = itemView.findViewById(R.id.editButton);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.one_comment, parent, false);
+        return new CommentViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentViewHolder holder, final int position) {
+        final String comment = comments.get(position);
+
+        holder.userNameTextView.setText(getAuthor());
+
+        holder.profileImageView.setImageBitmap(getProfilepic());
+        holder.textViewComment.setText(comment);
+
+        // Set click listener for the delete button
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Remove the comment from the list
+                comments.remove(position);
+                // Notify adapter that item is removed
+                notifyItemRemoved(position);
+            }
+        });
+
+        // Set click listener for the edit button
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEditDialog(position, holder.itemView);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+
+    // Method to show an edit dialog
+    private void showEditDialog(final int position, View itemView) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+        builder.setTitle("Edit Comment");
+
+        // Set up the input
+        final EditText input = new EditText(itemView.getContext());
+        input.setText(comments.get(position));
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String editedComment = input.getText().toString();
+                comments.set(position, editedComment);
+                notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+}
